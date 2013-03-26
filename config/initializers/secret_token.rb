21ae1ccb46459cddd76919f018ea71c312d4586e
Ding <puhang.ding@brilliantech-solution.com>
2013-03-26 18:45:26 +0800

    ding: EPM Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EPM::Application.config.secret_token = '356b4af0c2f4cd8ffa783877d5bac694db29c0bbd91b3c4a1752fee22944d4d49bc759f1e6e7fb781c61632f79430a382c0c4efac764052907dbd00468c1a24d'
