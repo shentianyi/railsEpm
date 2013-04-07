86b0ed4f9ee18239b7c6119d0b274fcae621f6b0
Ding <puhang.ding@brilliantech-solution.com>
2013-04-07 10:58:47 +0800

    ding: auth code

--- config/initializers/thrift_config.rb
+++ config/initializers/thrift_config.rb
@@ -1,6 +1,6 @@
-require 'thrift/hbase_types'
-require 'thrift/hbase_constants'
-require 'thrift/service'
+# require 'thrift/hbase_types'
+# require 'thrift/hbase_constants'
+# require 'thrift/service'
 
 
 # transport = ::Thrift::BufferedTransport.new(::Thrift::Socket.new('192.168.0.178', '9001'))
