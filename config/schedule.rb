48c468aa90f34492614b7af95e4f093a1180f7e4
Ding <puhang.ding@brilliantech-solution.com>
2013-04-09 20:05:53 +0800

    ding: cron time

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@
 # After changes, do below:
 # whenever --update-crontab epm  --set environment=development
 #
-every 1.minute do # Many shortcuts available: :hour, :day, :month, :year, :reboot
+every 10.minutes do # Many shortcuts available: :hour, :day, :month, :year, :reboot
   # command "cd /home/ding/EPM && echo EPM__test: $(pwd) >> /home/ding/EPM_test"
   rake 'cron:rand'
 end
