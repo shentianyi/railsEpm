20997701096c42c4a97fc1436eacc8e1c7a17aab
tianyi <tianyi@BingqingtekiMac-mini-2.local>
2013-07-12 18:43:42 +0800

    preview version, comment user confirmation

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
   skip_before_filter :require_user,:only=>[:new,:create]
   skip_before_filter :require_active_user,:only=>[:new,:create]
   skip_before_filter :check_tenant_status
-    skip_before_filter :find_current_user_tenant,:only=>[:new,:create]
+  skip_before_filter :find_current_user_tenant,:only=>[:new,:create]
   skip_authorize_resource :only=>[:new,:create]
   before_filter :require_no_user, :only => [:new, :create]
 
@@ -27,7 +27,7 @@ class SubscriptionsController < ApplicationController
       @user.deliver_user_confirmation!
       flash[:notice] = 'Your account has been created.Please finish the process with the mail confirmation'
 
-      redirect_to new_user_confirmations_url
+      redirect_to new_user_sessions_url#new_user_confirmations_url
     rescue ActiveRecord::RecordInvalid => invalid
       flash[:notice]='Account create failed'
       render :action=>:new
