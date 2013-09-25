981e53cfb1b5c06fdbebb36697931d03ab82adeb
wayne <zixiao.wang@cz-tek.com>
2013-09-25 18:41:52 +0800

    wzx

--- app/assets/javascripts/Manage-view.js
+++ app/assets/javascripts/Manage-view.js
@@ -5,3 +5,13 @@
  * Time: 下午4:04
  * To change this template use File | Settings | File Templates.
  */
+var MANAGE=MANAGE || {};
+MANAGE.view={};
+MANAGE.view.init=function(){
+    $("#entity-add-show").on("click",function(){
+
+    });
+    $("#close-add-entity").on("click",function(){
+
+    });
+};
