aa72aeeaad2165c6dabbb0c1fe059fd278195387
Wang Song <song.wang@brilliantech-solution.com>
2013-09-25 17:35:58 +0800

    translate

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -8,26 +8,23 @@ class SubscriptionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :is_sign_up_allowed, :only => [:new,:create]
   layout 'non_authorized'
-
   def new
-
-    @title= 'Sign up'
-     render
+    @title= I18n.t 'auth.view.sign_title'
+    render
   end
 
   def is_sign_up_allowed
-        if !$open_signup
-          flash[:notice]="我们还没有开放注册"
-          render "errors/500"
-        else
-          return true
-        end
+    if !$open_signup
+      flash[:notice]="我们还没有开放注册"
+      render "errors/500"
+    else
+    return true
+    end
   end
-
 
   def create
     begin
-      raise(ArgumentError,"email has been token")  if $invalid_emails.include?(params[:email])
+      raise(ArgumentError, I18n.t('auth.msg.email_token'))  if $invalid_emails.include?(params[:email])
       @user=User.new
       @user = @user.create_tenant_user!(params[:email],
                                         params[:password],
@@ -35,18 +32,16 @@ class SubscriptionsController < ApplicationController
                                         params[:company_name])
 
       @user.deliver_user_confirmation!
-      flash[:notice] = 'Your account has been created.Please finish the process with the mail confirmation'
-
+      # flash[:notice] = 'Your account has been created.Please finish the process with the mail confirmation'
+      flash[:notice] = I18n.t 'auth.msg.sign_success'
       redirect_to new_user_sessions_url#new_user_confirmations_url
     rescue ArgumentError=>invalid
-         flash[:notice]='email has been token'
+      flash[:notice]=I18n.t 'auth.msg.email_token'
       render :action=>:new
     rescue ActiveRecord::RecordInvalid => invalid
-      flash[:notice]='Account create failed'
+      flash[:notice]=I18n.t 'auth.msg.sign_fail'
       render :action=>:new
     end
   end
-
-
 
 end
