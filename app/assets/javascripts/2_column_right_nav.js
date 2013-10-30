36d08aae92a8333b9e6ccd6542ba1872cfc2de64
wayne <zixiao.wang@cz-tek.com>
2013-10-30 12:21:06 +0800

    w

--- app/assets/javascripts/2_column_right_nav.js
+++ app/assets/javascripts/2_column_right_nav.js
@@ -10,9 +10,25 @@ right_nav_main.init=function(){
     right_nav_main.initial.height();
     right_nav_main.initial.width();
     right_nav_main.initial.name();
-    right_nav_main.item.add_init();
+    right_nav_main.item_prototype.add_init();
+    right_nav_main.item_prototype.delete_init();
+    right_nav_main.item_prototype.edit_init();
+    right_nav_main.observe_count.count=$("#content-right-nav-group").children().length;
+    right_nav_main.observe_count.observe();
 };
 right_nav_main.current_section=$("#content-right-nav").attr("name");
+right_nav_main.item_prototype={};
+right_nav_main.observe_count={
+    observe:function(){
+        if(this.count==0){
+            $("#left-content-title>p,#left-content-title>i").css("display","none");
+        }
+        else{
+            $("#left-content-title>p,#left-content-title>i").css("display","inline-block");
+        }
+    }
+};
+right_nav_main.dashboard={};
 //////////////////////////////////////////////////initial
 right_nav_main.initial={}
 right_nav_main.initial.height=function(){
@@ -40,39 +56,38 @@ right_nav_main.initial.name=function(){
     $("#current-group-item").text(name);
 }
 ////////////////////////////////////////////////// add item
-right_nav_main.item={};
-right_nav_main.item.add=function(){};
-right_nav_main.item.add.prototype={
+right_nav_main.item_prototype.add=function(){};
+right_nav_main.item_prototype.add.prototype={
     add_show:function(){
-        $("#manage-left-menu li:nth-of-type(2) span").css("left","999em");
-        $("#manage-left-menu li:nth-of-type(2) input").val("").css("left","8px").attr("placeholder",this.name).focus();
+        $("#content-right-nav-add-block").css("left","auto");
+        $("#content-right-nav-add-block>input").focus();
     },
     add_complete:function(){
         var validate=false;
-        var name=$("#manage-menu-add input").val();
+        var name=$("#content-right-nav-add-block>input").val();
         if($.trim(name).length==0){
-            MANAGE.manage_menu_left_add.prototype.add_hide();
+            right_nav_main.item_prototype.add.prototype.add_hide();
         }
         else{
-            $("#manage-left-menu li").each(function(){
-                if($("#manage-menu-add input").val()!=$(this).attr("title") ){
+            $("#content-right-nav-group>li").each(function(){
+                if(name!=$(this).attr("title") ){
                     validate=true;
                 }
                 else{
                     validate=false;
-                    return false
+                    return false;
                 }
             });
             if(validate){
 
-
-//                  $("#manage-left-menu").append($("<li />").attr("title",name)
-//                      .append($("<i />").addClass("icon-trash icon-item")).append($("<a />").text(name)));
-//                  $("#manage-menu-add input").val("");
-//                  MANAGE.manage_menu_left_add.prototype.add_hide();
-//                  MANAGE.left_count++;
-//                  MANAGE.left_count_observable();
-
+//                  $("#content-right-nav-group").append($("<li />").attr("title",name)
+//                      .append($("<a />").text(name))
+//                      .append($("<i />").addClass("icon-remove"))
+//                  );
+//                  $("#content-right-nav-add-block>input").val("");
+//                  right_nav_main.item_prototype.add.prototype.add_hide();
+//                  right_nav_main.observe_count.count++;
+//                  right_nav_main.observe_count.observe();
 
 
                 var href=this.href;
@@ -82,20 +97,18 @@ right_nav_main.item.add.prototype={
                     }
                 }, function(data) {
                     if(data.result) {
-                        $("#manage-left-menu").append($("<li />").attr("title",name).attr("number", data.object)
-                            .append($("<i />").addClass("icon-trash icon-item")).append($("<a href='"+href + data.object + "'/>").text(name)));
-                        $("#manage-menu-add input").val("");
-                        MANAGE.manage_menu_left_add.prototype.add_hide();
-                        MANAGE.left_count++;
-                        MANAGE.left_count_observable();
+                        $("#content-right-nav-group").append($("<li />").attr("title",name).attr("number", data.object)
+                            .append($("<a href='"+href + data.object + "'/>").text(name))
+                            .append($("<i />").addClass("icon-remove"))
+                        );
+                        $("#content-right-nav-add-block>input").val("");
+                        right_nav_main.item_prototype.add.prototype.add_hide();
+                        right_nav_main.observe_count.count++;
+                        right_nav_main.observe_count.observe();
                     } else {
                         MessageBox(data.content,"top","warning");
                     }
                 });
-
-
-
-
 
             }
             else{
@@ -107,46 +120,186 @@ right_nav_main.item.add.prototype={
 
     },
     add_hide:function(){
-        $("#manage-left-menu li:nth-of-type(2) span").css("left","0px");
-        $("#manage-left-menu li:nth-of-type(2) input").val("").css("left","-999em");
+        $("#content-right-nav-add-block").css("left","-999em");
+        $("#content-right-nav-add-block>input").blur();
     },
-    constructor:right_nav_main.item.add
+    constructor:right_nav_main.item_prototype.add
 
 }
-
 
 function dashboard_add(){
     this.name="dashboard";
     this.href="../dashboards?p=";
     this.postHref='../dashboards';
 }
-dashboard_add.prototype=right_nav_main.item.add.prototype;
+dashboard_add.prototype=right_nav_main.item_prototype.add.prototype;
 dashboard_add.prototype.constructor=dashboard_add;
+right_nav_main.dashboard.add=new dashboard_add();
 
-MANAGE.dashboard.add=new dashboard_add();
 
-
-right_nav_main.item.add_init=function(){
-    $("#manage-menu-add").on("click",function(){
-        MANAGE[MANAGE.type].add.add_show();
+right_nav_main.item_prototype.add_init=function(){
+    $("#content-right-nav-add-button").on("click",function(){
+        right_nav_main[right_nav_main.current_section].add.add_show();
     });
-    $("#manage-menu-add input").on("keydown",function(event){
+    $("#content-right-nav-add-block>input").on("keydown",function(event){
         if(adapt_event(event).event.keyCode==13){
             if($.trim($(adapt_event(event).target).val())==0){
                 MessageBox("it needs a name","top","warning");
             }
             else{
-                MANAGE[MANAGE.type].add.add_complete();
+                right_nav_main[right_nav_main.current_section].add.add_complete();
             }
         }
         else if(adapt_event(event).event.keyCode==27){
-            MANAGE[MANAGE.type].add.add_complete();
+            right_nav_main[right_nav_main.current_section].add.add_hide();
         }
     });
-    $("#manage-menu-add input").blur(function(){
-        MANAGE[MANAGE.type].add.add_complete();
-    });
-};
 
+};
+////////////////////////////////////////////////// remove item
+right_nav_main.item_prototype.delete=function(){};
+right_nav_main.item_prototype.delete.prototype={
+    delete_complete:function(e){
+
+//        $(e.target).parent().remove();
+//        right_nav_main.observe_count.count--;
+//        right_nav_main.observe_count.observe();
+
+
+        var number = $(e.target).parent().attr("number");
+        var local=this.local
+        $.ajax({
+            url : this.url+number,
+            type : 'DELETE',
+            success : function(data) {
+                if(data.result){
+                    window.location.href = local;
+                    right_nav_main.observe_count.count--;
+                    right_nav_main.observe_count.observe();
+                }
+                else{
+                    MessageBox(data.content,"top","warning");
+                }
+            }
+        });
+
+
+    },
+    constructor:right_nav_main.item_prototype.delete
+}
+
+function dashboard_delete(){
+    this.url="../dashboards/";
+    this.local= "../dashboards";
+    this.name="dashboard";
+}
+dashboard_delete.prototype=right_nav_main.item_prototype.delete.prototype;
+dashboard_delete.prototype.constructor=dashboard_delete;
+
+right_nav_main.dashboard.delete=new dashboard_delete();
+
+right_nav_main.item_prototype.delete_init=function(){
+    $("#content-right-nav-group").on("click","i.icon-remove",function(event){
+        if(confirm(I18n.t('view.manage.base.delete_confirm'))){
+            var e = adapt_event(event).event;
+            right_nav_main[right_nav_main.current_section].delete.delete_complete(e);
+        }
+    });
+}
+////////////////////////////////////////////////// edit item
+right_nav_main.item_prototype.edit=function(){};
+right_nav_main.item_prototype.edit.prototype={
+    edit_show:function(e){
+        var x= e.clientX-10,
+            y= e.clientY-55,
+            name=$("#left-content-title>p").text();
+        $("#content-left-content-edit-block").css("left",x).css("top",y);
+        $("#content-left-content-edit-block>input").val(name).focus();
+    },
+    edit_check:function(e){
+            var validate=true;
+            if($(e.target).val()==$("#content-right-nav-group>li.active").attr("title")){
+                this.edit_hide();
+                return false;
+            }
+            else{
+                $("#content-right-nav-group>li").each(function(){
+                    if($(this).attr("title")==$(e.target).val()){
+                        validate=false;
+                        MessageBox("Same name exist yet","top","warning");
+                        return false;
+                    }
+                });
+                return validate;
+            }
+    },
+    edit_update:function(){
+        var id=$("#content-right-nav-group>li.active").attr("number");
+        var name=$("#content-left-content-edit-block>input").val();
+
+//            $("#content-right-nav-group>li.active>a").text(name);
+//            $("#content-right-nav-group>li.active").attr("title",name);
+//            $("#current-group-item").text(name);
+//            this.edit_hide();
+
+
+
+        $.ajax({
+            url : this.url,
+            type : 'PUT',
+            data : {
+                id:id,
+                data:{
+                    name:name
+                }
+            },
+            success : function(data) {
+                if(data){
+                    $("#content-right-nav-group>li.active>a").text(name);
+                    $("#content-right-nav-group>li.active").attr("title",name);
+                }
+            }
+        });
+        this.edit_hide();
+
+    },
+    edit_hide:function(){
+        $("#content-left-content-edit-block").css("left","-999em");
+        $("#content-left-content-edit-block>input").val("").blur();
+    },
+    constructor:right_nav_main.item_prototype.edit
+}
+
+function dashboard_edit(){
+    this.url = "../dashboards";
+}
+dashboard_edit.prototype=right_nav_main.item_prototype.edit.prototype;
+dashboard_edit.prototype.constructor=dashboard_edit;
+
+
+right_nav_main.dashboard.edit=new dashboard_edit();
+
+right_nav_main.item_prototype.edit_init=function(){
+    $("body").on("click","#current-group-item-edit",function(event){
+        var e=adapt_event(event).event;
+        right_nav_main[right_nav_main.current_section].edit.edit_show(e);
+    });
+    $("#content-left-content-edit-block").on("keydown","input",function(event){
+        var e=adapt_event(event).event;
+        if(e.keyCode==13){
+            if($.trim($(e.target).val())==0){
+                MessageBox("please don\'t leave it empty","top","warning");
+            }
+            else{
+                if(right_nav_main[right_nav_main.current_section].edit.edit_check(e)){
+                    right_nav_main[right_nav_main.current_section].edit.edit_update()
+                }
+            }
+        }
+        else if(e.keyCode==27){
+            right_nav_main[right_nav_main.current_section].edit.edit_hide();
+        }
+    });
+}
 
 
