a1b690cf2a9954f18e4f58ca42f7d5409358ed95
wayne <zixiao.wang@cz-tek.com>
2013-12-27 15:36:28 +0800

    fix

--- app/assets/javascripts/Index.js
+++ app/assets/javascripts/Index.js
@@ -10,21 +10,21 @@
         $.get("/welcome/users",{},function(data){
             if(data.result){
                 CHARTBOOT.pie.scores=[data.content.user,data.content.manager,data.content.directors,data.content.admin];
+                CHARTBOOT.generatePie(CHARTBOOT.pie.scores,"chart-pie-wrap");
+                $("#count-general").text(CHARTBOOT.pie.scores[0]);
+                $("#count-department-manager").text(CHARTBOOT.pie.scores[1]);
+                $("#count-general-manager").text(CHARTBOOT.pie.scores[2]);
+                $("#count-admin").text(CHARTBOOT.pie.scores[3]);
             }
         });
         $.get("/welcome/statistics",{},function(data){
             if(data.result){
                 CHARTBOOT.column.scores=[data.content.kpi,data.content.entity,data.content.user,data.content.view];
+                CHARTBOOT.generateColumn(CHARTBOOT.column.scores,"chart-column-wrap");
             }
         });
 //        CHARTBOOT.pie.scores=[10,20,30,40];
 //        CHARTBOOT.column.scores=[30,20,30,40];
-        CHARTBOOT.generatePie(CHARTBOOT.pie.scores,"chart-pie-wrap");
-        CHARTBOOT.generateColumn(CHARTBOOT.column.scores,"chart-column-wrap");
-        $("#count-general").text(CHARTBOOT.pie.scores[0]);
-        $("#count-department-manager").text(CHARTBOOT.pie.scores[1]);
-        $("#count-general-manager").text(CHARTBOOT.pie.scores[2]);
-        $("#count-admin").text(CHARTBOOT.pie.scores[3]);
         //$("#user-email").text(" "+"superxiao21@sina.com");
         var login_time=new Date().toWayneString().minute;
         $("#login-time").text(" "+login_time);
