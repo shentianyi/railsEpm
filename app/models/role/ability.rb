2b460914f9a4c9455cea779f8c617c6ee8c1a24e
Charlot <7wangsong@gmail.com>
2013-12-31 15:20:24 +0800

    ab

--- app/models/role/ability.rb
+++ app/models/role/ability.rb
@@ -8,8 +8,8 @@ class Ability
       can :read,:all
     elsif Role.manager?(user.role_id)
       can :manage,User,:entity_id=>user.entity_id
-      can :manage,Entity,:id=>user.entity_id
-      can :manage,[Kpi,UserSession]
+      # can :manage,Entity,:id=>user.entity_id
+       can :manage,[Entity,EntityGroup,EntityGroupItem,Kpi,KpiEntry,KpiItem,User,UserKpiItem,UserSession]
       can :read, KpiCategory,:tenant_id=>user.tenant_id
       can :read,:all
     elsif Role.user?(user.role_id)
