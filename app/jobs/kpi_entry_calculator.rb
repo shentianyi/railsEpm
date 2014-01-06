4b211e5c9b30d7aeb7240d58069987ebb55baa36
Charlot <7wangsong@gmail.com>
2014-01-06 11:42:50 +0800

    remove put

--- app/jobs/kpi_entry_calculator.rb
+++ app/jobs/kpi_entry_calculator.rb
@@ -2,7 +2,6 @@
 class KpiEntryCalculator
   @queue='kpi_entry_calculate_queue'
   def self.perform kpi_entry_id
-    puts "--------------"
     KpiEntriesHelper.calculate_kpi_parent_value kpi_entry_id
   end
 end
