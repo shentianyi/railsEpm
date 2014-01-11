dc6ce7f427e4e6b35bed7a0bd37375df3b7b2991
tesla lee <leechee89@gmail.com>
2014-01-11 16:26:19 +0800

    db

--- db/schema.rb
+++ db/schema.rb
@@ -128,10 +128,9 @@ ActiveRecord::Schema.define(:version => 20140110102739) do
   create_table "entity_groups", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
-    t.boolean  "is_public",   :default => false
-    t.string   "description"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "is_public",  :default => false
     t.string   "code"
   end
 
