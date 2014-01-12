216ff7168abda0486c7ac1fd1360fd09aa785baa
Charlot <7wangsong@gmail.com>
2014-01-12 16:20:14 +0800

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140110102739) do
+ActiveRecord::Schema.define(:version => 20140112062513) do
 
   create_table "admin_kpi_category_templates", :force => true do |t|
     t.string   "name"
@@ -128,9 +128,10 @@ ActiveRecord::Schema.define(:version => 20140110102739) do
   create_table "entity_groups", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "is_public",  :default => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.boolean  "is_public",   :default => false
+    t.string   "description"
     t.string   "code"
   end
 
--- script/import_data/import_entity_kpi_range.rb
+++ script/import_data/import_entity_kpi_range.rb
@@ -3,6 +3,7 @@ Dir["#{ARGV[0]}/*.csv"].each do |f|
   puts "importing: #{f}"
   CSV.foreach(f,:headers=>true,:col_sep=>';') do |row|
     if user_kpi_item=UserKpiItem.where(:kpi_id=>row["KPIID"],:entity_id=>row["EntityID"]).first
+      puts row["Max"]
        user_kpi_item.update_attributes(:target_max=>row["Max"],:target_min=>row["Min"])
     end
   end
