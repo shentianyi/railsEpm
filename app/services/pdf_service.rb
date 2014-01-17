824af0f1c79e28923446bf938ca6a3f68b0dc754
tesla lee <leechee89@gmail.com>
2014-01-17 18:04:49 +0800

    fix

--- app/services/pdf_service.rb
+++ app/services/pdf_service.rb
@@ -4,6 +4,6 @@ class PdfService
     WickedPdf.new.pdf_from_string(ActionController::Base.new().render_to_string('templates/analysis_pdf',
                                                                                 :layout => "pdfs/analysis",
                                                                                 :locals => {:datas => datas}
-                                                                                ))
+                                  ))
   end
 end
