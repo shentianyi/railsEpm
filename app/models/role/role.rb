0489d54d0dc4a327fb6a4b64803d8396a67f59f3
Charlot <7wangsong@gmail.com>
2014-02-11 17:04:07 +0800

    on user ablity

--- app/models/role/role.rb
+++ app/models/role/role.rb
@@ -1,12 +1,13 @@
 #encoding: utf-8
 class Role
+  RoleMethods=[:admin?,:manager?,:director?,:user?]
   @@roles={:'100'=>{:name=>'user',:display=>(I18n.t 'manage.user.role.user')},
                     # :'200'=>{:name=>'manager',:display=>(I18n.t 'manage.user.role.manager')},
                     :'300'=>{:name=>'director',:display=>(I18n.t 'manage.user.role.director')},
                     :'400'=>{:name=>'admin',:display=>(I18n.t 'manage.user.role.admin')}}
 
   class<<self
-    [:admin?,:manager?,:director?,:user?].each do |m|
+    RoleMethods.each do |m|
       define_method(m){|id|
         @@roles[id_sym(id)][:name]==m.to_s.sub(/\?/,'')
       }
