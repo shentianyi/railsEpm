1f7944ac5792cc75b09e4d624b903a7383827058
tesla lee <leechee89@gmail.com>
2014-02-19 18:09:52 +0800

    dashboard fast import

--- script/import_data/import_dashboard_fast.rb
+++ script/import_data/import_dashboard_fast.rb
@@ -1,8 +1,9 @@
 require 'csv'
 #create dashboard
 ActiveRecord::Base.transaction do
-  puts "start importing a.csv"
+  puts "start importing"
   dashboards = {}
+  entity_groups = {}
   CSV.foreach("#{ARGV[0]}/a.csv", :headers => true, :col_sep => ';') do |row|
     params= {}
     if user = User.find_by_email(row["email"])
@@ -11,6 +12,7 @@ ActiveRecord::Base.transaction do
       dashboard = Dashboard.new(params)
       if dashboard.save!
         dashboards[row["code"]] = dashboard
+        entity_groups[row["code"]] = row["entitygroups"]
       else
         puts "Dashboard with email:"+row["email"]+" name:"+row["name"] + "created failed!";
       end
@@ -18,8 +20,6 @@ ActiveRecord::Base.transaction do
       puts "User with: "+row["email"] + "not found!"
     end
   end
-  puts dashboards
-  puts "finish importing a.csv"
 
   #create dashboard item
   CSV.foreach("#{ARGV[0]}/b.csv", :headers => true, :col_sep => ';') do |row|
@@ -27,30 +27,56 @@ ActiveRecord::Base.transaction do
     if dashboards.has_key? row["belongs_to"]
 
       #for each kpis
-      row["kpis"].split("|").each do |c|
+      row["kpis"].split("|").each do |kpi|
         params[:dashboard_id] = dashboards[row["belongs_to"]].id
         params[:interval] = row["interval"]
-        params[:title] = c
+        params[:title] = kpi
         params[:chart_type]=row["chart_type"]
         params[:sizex] = row["sizex"]
         params[:sizey] = row["sizey"]
         dashboard_item = DashboardItem.new(params)
         if dashboard_item.save!
-          dashboarditems[row["code"]] = dashboard_item
+          groups = entity_groups[row["belongs_to"]].split("+")
+
+          if groups.last.include?("|")
+            #if we have "|"
+            list_groups = groups.last.split("|")
+            groups.delete(groups.last)
+
+            list_groups.each do |all|
+
+            end
+          else
+            #if we do not have "|"
+            groups.each do |g|
+              if (k = Kpi.find_by_name(kpi)) && (entity_group = EntityGroup.find(g))
+                param = {}
+                param[:entity_group] = entity_group.id
+                param[:kpi_id] = k.id
+                param[:dashboard_item_id] = dashboard_item.id
+                param[:calculate_type] = row["calculate_type"]
+                param[:time_string] = row["time_string"]
+                param[:count] = 1
+                condition = DashboardCondition.new(param)
+
+                if condition.save!
+
+                else
+                  puts "condition save failed with dashboard_item_id"+dashbord_item.id
+                end
+
+              else
+                puts "Kpi not found with name:"+k+ "or entity_group not found with id "+g
+              end
+            end
+          end
         else
           puts "Dashboard Item created failed wit code:"+row["code"]
         end
-
-        entity_groups = row["entitygroups"].split("+")
-        if entity_groups.last.include?("|")
-        else
-
-        end
-
       end
     else
       puts "Dashboard not exit with code:"+row["belongs_to"]
     end
   end
-  puts dashboarditems
+  puts "end importing"
 end
