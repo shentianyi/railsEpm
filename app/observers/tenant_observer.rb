cff68a4743d4c5abb94ed857b35af65c4350a0bd
Charlot <7wangsong@gmail.com>
2014-03-03 17:36:41 +0800

    fix bugs

--- app/observers/tenant_observer.rb
+++ app/observers/tenant_observer.rb
@@ -2,26 +2,25 @@
 class TenantObserver<ActiveRecord::Observer
 
   observe :tenant
+
   def before_create tenant
     tenant.access_key=SecureRandom.uuid
     # create default kpi category
     tenant.kpi_categories<<KpiCategory.new
     # create default entity
     # set super user's entity be default entity
-    entity=Entity.new(:name=>tenant.company_name.length==0 ? 'Default Entity' : tenant.company_name)
+    entity=Entity.new(:name => tenant.company_name.length==0 ? 'Default Entity' : tenant.company_name)
     tenant.super_user.entity = entity
     tenant.super_user.role_id=400
-<<<<<<< HEAD
-    #tenant.entities<<entity
-    department=tenant.super_user.create_departs.build(:name=>tenant.company_name.length==0 ? 'Default Entity Group' : tenant.company_name)
-    department.save
-    department.entity_group.entities << entity
-=======
     tenant.entities<<entity
-    entity_group=tenant.super_user.entity_groups.build(:name=>tenant.company_name.length==0 ? 'Default Entity' : tenant.company_name)
-    entity_group.entities<<entity
->>>>>>> e8cc1d6ecf8081ea2d645b846bbe024a15dd0822
-  #
+
+    department=Department.new(:name => tenant.company_name.length==0 ? 'Default Department' : tenant.company_name)
+    department.creator =tenant.super_user
+    tenant.departments<<department
+
+    #department.entity_group.entities << entity
+    department.default_entity=entity
   end
+
 
 end
