df532ab7b2f4da3570b7e9a698ab35179dd7ccb9
liqites <leechee89@gmail.com>
2014-03-11 16:11:18 +0800

    setting

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -2,9 +2,14 @@ class Admin::SettingsController < Admin::ApplicationController
 
 	def version_save
 		#save version
+    msg = Message.new
+    Setting.new(:ios_app_version => params[:version],:ios_app_update_is_option=> params[:optional]).save
+    msg.result = true
+    render :json=>msg
 	end
 
 	def version
 		#get version
+    @setting = Setting.find
 	end
 end
