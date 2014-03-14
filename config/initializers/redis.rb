31340678fa5d96c60ad71da45e23a0e8dd999c23
Charlot <7wangsong@gmail.com>
2014-03-14 12:02:28 +0800

    rechange redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,14 +2,14 @@ require 'rake'
 require "resque/server"
 require "resque/tasks"
 
-$redis=Redis.new(:host => "127.0.0.1",:port => "6379",:db=>7)
+$redis=Redis.new(:host => "127.0.0.1",:port => "6379",:db=>6)
 
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
       $redis.client.disconnect
-      $redis=Redis.new(:host => "127.0.0.1",:port => "6379",:db=>7)
+      $redis=Redis.new(:host => "127.0.0.1",:port => "6379",:db=>6)
     end
   end
 end
