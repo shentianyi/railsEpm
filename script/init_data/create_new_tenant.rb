e1c3eb89dc2656428dc3f4b5f171e56cae8196b4
Charlot <7wangsong@gmail.com>
2014-03-20 16:37:32 +0800

    fix script for adding tenant

--- script/init_data/create_new_tenant.rb
+++ script/init_data/create_new_tenant.rb
@@ -7,7 +7,7 @@ puts arr
 if arr.count==3
   user=User.new
   user.status=0
-  user.create_tenant_user!(ARGV[0], ARGV[1], ARGV[1], ARGV[2])
+  user.create_tenant_user!(arr[0], arr[1], arr[1], arr[2])
   puts "email: #{arr[0]}, password: #{arr[1]}, company name: #{arr[2]}"
 else
   puts 'params error!'.red
