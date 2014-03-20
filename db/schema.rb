e4d174067a56c9bde801aacd58f789f7f1252316
tesla lee <leechee89@gmail.com>
2014-03-20 12:32:46 +0800

    db

--- db/schema.rb
+++ db/schema.rb
@@ -251,7 +251,7 @@ ActiveRecord::Schema.define(:version => 20140319061208) do
     t.integer  "kpi_category_id"
     t.integer  "unit"
     t.integer  "frequency"
-    t.float    "target_max"
+    t.float    "target_max",      :default => 0.0
     t.boolean  "is_calculated",   :default => false
     t.integer  "direction"
     t.integer  "period"
@@ -267,48 +267,6 @@ ActiveRecord::Schema.define(:version => 20140319061208) do
   add_index "kpis", ["kpi_category_id"], :name => "index_kpis_on_kpi_category_id"
   add_index "kpis", ["tenant_id"], :name => "index_kpis_on_tenant_id"
   add_index "kpis", ["user_id"], :name => "index_kpis_on_user_id"
-
-  create_table "oauth_access_grants", :force => true do |t|
-    t.integer  "resource_owner_id", :null => false
-    t.integer  "application_id",    :null => false
-    t.string   "token",             :null => false
-    t.integer  "expires_in",        :null => false
-    t.text     "redirect_uri",      :null => false
-    t.datetime "created_at",        :null => false
-    t.datetime "revoked_at"
-    t.string   "scopes"
-  end
-
-  add_index "oauth_access_grants", ["token"], :name => "index_oauth_access_grants_on_token", :unique => true
-
-  create_table "oauth_access_tokens", :force => true do |t|
-    t.integer  "resource_owner_id"
-    t.integer  "application_id"
-    t.string   "token",             :null => false
-    t.string   "refresh_token"
-    t.integer  "expires_in"
-    t.datetime "revoked_at"
-    t.datetime "created_at",        :null => false
-    t.string   "scopes"
-  end
-
-  add_index "oauth_access_tokens", ["refresh_token"], :name => "index_oauth_access_tokens_on_refresh_token", :unique => true
-  add_index "oauth_access_tokens", ["resource_owner_id"], :name => "index_oauth_access_tokens_on_resource_owner_id"
-  add_index "oauth_access_tokens", ["token"], :name => "index_oauth_access_tokens_on_token", :unique => true
-
-  create_table "oauth_applications", :force => true do |t|
-    t.string   "name",         :null => false
-    t.string   "uid",          :null => false
-    t.string   "secret",       :null => false
-    t.text     "redirect_uri", :null => false
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.integer  "owner_id"
-    t.string   "owner_type"
-  end
-
-  add_index "oauth_applications", ["owner_id", "owner_type"], :name => "index_oauth_applications_on_owner_id_and_owner_type"
-  add_index "oauth_applications", ["uid"], :name => "index_oauth_applications_on_uid", :unique => true
 
   create_table "tenants", :force => true do |t|
     t.string   "company_name",           :null => false
