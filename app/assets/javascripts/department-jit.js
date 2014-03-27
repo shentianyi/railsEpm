31d3d571ee7981b859f6b22dccc8d1c998c78b31
tesla lee <leechee89@gmail.com>
2014-03-27 18:26:28 +0800

    oritentaion

--- app/assets/javascripts/department-jit.js
+++ app/assets/javascripts/department-jit.js
@@ -59,6 +59,7 @@ function init(dep){
     //levels
     levelsToShow: 3,  
     //enable panning
+    orientation: "top",
     Navigation: {
       enable:true,
       panning:true
@@ -151,7 +152,11 @@ function init(dep){
   st.compute();
   //optional: make a translation of the tree
   st.geom.translate(new $jit.Complex(-200, 0), "current");
+  //
+
   //emulate a click on the root node.
   st.onClick(st.root);
+  //
+
   //end
 }
