2213138cb26bb6c6ff69a79ab25655853ee95a9a
Charlot <7wangsong@gmail.com>
2014-04-08 10:32:31 +0800

    recall

--- db/migrate/20140408020927_change_entity_group_default_value.rb
+++
@@ -1,9 +0,0 @@
-class ChangeEntityGroupDefaultValue < ActiveRecord::Migration
-  def up
-    change_column :entity_groups,:code,:string,:default => ''
-    change_column :entity_groups,:description,:string,:default => ''
-  end
-
-  def down
-  end
-end
