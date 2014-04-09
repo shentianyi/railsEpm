4f9d0ca3c175898dfe1767e1c4eaf31552640e40
liqites <leechee89@gmail.com>
2014-04-10 07:51:07 +0800

    input filter

---
+++ app/services/entry/input_filter.rb
@@ -0,0 +1,10 @@
+module Entry
+	class InputFilter
+		#record: on line of record,
+		#hash parameters with kpis and attributes
+		#kpis: selected kpis,used to pick out kpi
+		def filter record,kpis
+			
+		end
+	end
+end
