bf51a51fd6610284c60ae339689aeb7bf5251e37
tesla lee <leechee89@gmail.com>
2014-04-15 11:53:05 +0800

    kpi property

--- app/models/kpi_property.rb
+++ app/models/kpi_property.rb
@@ -7,4 +7,13 @@ class KpiProperty < ActiveRecord::Base
   has_many :kpi_property_values, :through =>:kpi_property_items, :dependent => :destroy
 
   acts_as_tenant(:tenant)
+
+  def self.property_values kpi_id,kpi_property_id
+    item = KpiPropertyItem.where("kpi_id = ? AND kpi_property_id = ?",kpi_id,kpi_property_id)
+    if item.nil?
+      nil
+    else
+      item.kpi_property_values.pluck(:value)
+    end
+  end
 end
