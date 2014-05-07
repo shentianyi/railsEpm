29b3bac7191bc3191154651012b4fd04e3ce6be0
liqites <leechee89@gmail.com>
2014-05-08 00:05:16 +0800

    kpi calculate queue

--- app/models/kpi/kpi_calculate_queue.rb
+++ app/models/kpi/kpi_calculate_queue.rb
@@ -1,39 +1,62 @@
 #encoding: utf-8
 class KpiCalcylateQueue
-  KPICALCULATENAME="kpi_calculate_queue"
-  cal_queue = {}
+  require 'singleton'
 
-  def self.push entry_id
-    cal_queue = $redis.get(KPICALCULATENAME)
-    if cal_queue.nil?
-      cal_queue = {}
+  KPICALCULATENAME="kpi_calculate_queue"
+  @cal_queue = {}
+
+  def initialize
+    @cal_queue = $redis.get(KPICALCULATENAME)
+    if @cal_queue.nil?
+      @cal_queue = {}
+      $redis.set(KPICALCULATENAME,@cal_queue)
     end
   end
 
-  def self.finish key
+  def self.push entry_id
+    entry = KpiEntry.find_by(entry_id: entry_id)
+    unless entry.nil?
+      if kpis = Kpi.parent_kpis_by_id(entry.kpi_id)
+        kpis.each do |k|
+          #q[redis_key(k.id,entry.parsed_entry_at)] = true
+          if @cal_queue[redis_key(k.id,entry.parsed_entry_at)].nil?
+            @cal_queue[redis_key(k.id,entry.parsed_entry_at)] = {}
+            @cal_queue[redis_key(k.id,entry.parsed_entry_at)]["status"] = KpiQueueStatus::WAIT
+            @cal_queue[redis_key(k.id,entry.parsed_entry_at)]["kpi_id"] = k.id
+            @cal_queue[redis_key(k.id,entry.parsed_entry_at)]["parsed_entry_at"] = entry.parsed_entry_at
+          elsif @cal_queue[redis_key(k.id,entry.parsed_entry_at)]["status"] != KpiQueueStatus::WAIT
+            @cal_queue[redis_key(k.id,entry.parsed_entry_at)]["status"] = KpiQueueStatus::WAIT
+          else
+            #Do nothing
+          end
+        end
+        $redis.set(KPICALCULATENAME,@cal_queue)
+      end
+    end
+  end
 
+  def self.finish kpi_id,parsed_entry_at
+    key = redis_key(kpi_id,parsed_entry_at)
+    @cal_queue[k]["status"] = KpiQueueStatus::FINISH
+    $redis.set(KPICALCULATENAME,@cal_queue)
   end
 
   def self.check
-    
+    @cal_queue.each do |k,val|
+      if val["status"] == KpiQueueStatus::WAIT
+        #push to sidekiq worker
+
+        #change status to PROCESSING
+        val["status"] == KpiQueueStatus::PROCESSING
+      end
+    end
+    $redis.set(KPICALCULATENAME,@cal_queue)
   end
 
   #============
   #private
   #============
   private
-
-  def push_to_queue q,entry_id
-    entry = KpiEntry.find_by(entry_id: entry_id)
-    unless entry.nil?
-      if kpis = Kpi.parent_kpis_by_id(entry.kpi_id)
-        kpis.each do |k|
-          q[redis_key(k.id,entry.parsed_entry_at)] = true
-        end
-        $redis.set(KPICALCULATENAME,q)
-      end
-    end
-  end
 
   def redis_key kpi_id,parsed_entry_at
     "#"+kpi_id.to_s+"-"+parsed_entry_at.to_s
