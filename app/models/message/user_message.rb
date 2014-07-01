30adfb0bfcd829b1591988931bbbee5c9ed5ecaf
Charlot <7wangsong@gmail.com>
2014-07-02 00:45:38 +0800

    fix message

--- app/models/message/user_message.rb
+++ app/models/message/user_message.rb
@@ -2,19 +2,19 @@
 require 'base_class'
 
 class UserMessage<CZ::BaseClass
-  attr_accessor :key, :type, :count, :content, :user_id
+  attr_accessor :key, :type, :count, :content, :user_id, :link
 
   def initialize(params={})
     super
     @count=1 if @count.blank?
-    @key = UserMessage.generate_key(@user_id, @type) if @key.blank?
+    @key = UserMessage.generate_key(@user_id, @type, params[:key_params]) if @key.blank?
   end
 
   def save
     if message=UserMessage.find(self.user_id, self.type)
       $redis.hset message.key, 'count', message.count.to_i+1
     else
-      $redis.hmset self.key, 'user_id', self.user_id, 'count', self.count, 'type', self.type
+      $redis.hmset self.key, 'user_id', self.user_id, 'link', self.link, 'content', self.content||'', 'count', self.count, 'type', self.type
     end
   end
 
@@ -25,8 +25,8 @@ class UserMessage<CZ::BaseClass
     end
   end
 
-  def self.generate_key(user_id, type)
-    "user:#{user_id}:message_box:#{type}"
+  def self.generate_key(user_id, type, key_params=nil)
+    UserMessageType.genereate_user_message_key(user_id ， type, key_params)
   end
 
   def self.add_story_set_message user_id
@@ -36,6 +36,7 @@ class UserMessage<CZ::BaseClass
   def self.add_subscription_message user_id
     UserMessage.new(user_id: user_id, type: UserMessageType::SUBSCRIBE_ALERT).save
   end
+
 
   def self.clean_story_set_message user_id
     if message=find(user_id, UserMessageType::ADD_TO_STROY_SET)
@@ -47,8 +48,9 @@ class UserMessage<CZ::BaseClass
     messages=[]
     UserMessageType.types.each do |type|
       if message=self.find(user_id, type)
-        cl= UserMessageType.content_and_link(type)
-        messages<<{count: message.count.to_i, content: cl[0], link: cl[1]}
+        cl= UserMessageType.content(type)
+        content = message.blank? cl : "#{cl}: #{message.content}"
+        messages<<{count: message.count.to_i, content: content, link: message.link}
       end
     end
     return messages
