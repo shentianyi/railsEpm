d81366a499ffdc1b53002dc8238b683baef2fb0a
tesla lee <leechee89@gmail.com>
2014-07-01 10:57:05 +0800

    fix

--- app/models/kpi_subscribe.rb
+++ app/models/kpi_subscribe.rb
@@ -1,8 +1,9 @@
 class KpiSubscribe < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessor :user_id,:tenant_id
+  attr_accessor :user_id,:tenant_id,:kpi_id
   belongs_to :user
   belongs_to :tenant
+  belogns_to :kpi
 
   has_one :chart_condition,  :as => :chartable, :dependent => :destroy
   has_many :kpi_subscribe_users, :dependent => :destroy
--- db/migrate/20140701021852_create_kpi_subscribes.rb
+++ db/migrate/20140701021852_create_kpi_subscribes.rb
@@ -2,10 +2,12 @@ class CreateKpiSubscribes < ActiveRecord::Migration
   def change
     create_table :kpi_subscribes do |t|
       t.references :user
+      t.references :kpi
       t.references :tenant
       t.timestamps
     end
     add_index :kpi_subscribes, :user_id
     add_index :kpi_subscribes, :tenant_id
+    add_idnex :kpi_subscribes, :kpi_id
   end
 end
