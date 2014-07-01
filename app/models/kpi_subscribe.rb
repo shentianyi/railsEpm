6e566e41e4d9f05dfaaa4bd3570d59effa11eb00
tesla lee <leechee89@gmail.com>
2014-07-01 10:58:29 +0800

    fix

--- app/models/kpi_subscribe.rb
+++ app/models/kpi_subscribe.rb
@@ -3,7 +3,7 @@ class KpiSubscribe < ActiveRecord::Base
   attr_accessor :user_id,:tenant_id,:kpi_id
   belongs_to :user
   belongs_to :tenant
-  belogns_to :kpi
+  belongs_to :kpi
 
   has_one :chart_condition,  :as => :chartable, :dependent => :destroy
   has_many :kpi_subscribe_users, :dependent => :destroy
