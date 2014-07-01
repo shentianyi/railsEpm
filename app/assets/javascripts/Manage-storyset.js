a69d4584351f6cd2577269cfeb2b502869a0f5e3
wayne <zixiao.wang@cz-tek.com>
2014-07-01 11:05:02 +0800

    project new

--- app/assets/javascripts/Manage-storyset.js
+++ app/assets/javascripts/Manage-storyset.js
@@ -1,6 +1,13 @@
 var MANAGE = MANAGE || {};
 MANAGE.story_set = {};
-
+(function(){
+    $(document).ready(function(){
+        $("bocyd")
+            .on("click","#story-create-btn",function(){
+                MANAGE.story_set.create();
+            })
+    })
+})()
 MANAGE.story_set.create = function(){
     var title = $("#storyset-title").val();
     var description = $("#storyset-description").val();
