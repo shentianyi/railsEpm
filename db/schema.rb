ddae326d2c1470fcc829f5966d93cb8ecd9b54a3
tesla lee <leechee89@gmail.com>
2014-07-02 15:39:27 +0800

    add is_alert to kpi_subscribes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140701064220) do
+ActiveRecord::Schema.define(:version => 20140702073512) do
 
   create_table "admin_kpi_category_templates", :force => true do |t|
     t.string   "name"
@@ -333,8 +333,9 @@ ActiveRecord::Schema.define(:version => 20140701064220) do
     t.integer  "user_id"
     t.integer  "kpi_id"
     t.integer  "tenant_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "is_alert",   :default => false
   end
 
   add_index "kpi_subscribes", ["kpi_id"], :name => "index_kpi_subscribes_on_kpi_id"
