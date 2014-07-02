bb7e5875b70e69d7fab599e2f55b742b32a37288
tesla lee <leechee89@gmail.com>
2014-07-02 10:56:42 +0800

    fix

--- app/assets/javascripts/Manage-story.js
+++ app/assets/javascripts/Manage-story.js
@@ -1,10 +1,19 @@
 var MANAGE = MANAGE || {};
 MANAGE.story = {};
 
-MANAGE.story.create = function(story){
-    $.post('/stories', {story: story}, function (data) {
-        if(data.result){
-
-        }
+MANAGE.story.create = function(story,callback){
+        $.post('/stories', {story: story}, function (data) {
+        callback(data)
     });
+}
+
+MANAGE.story.get_attachments = function(id){
+    var attachments = null;
+    if ($('#' + id).children().length > 0) {
+        attachments = [];
+        $('#' + id).children().each(function () {
+            attachments.push({oriName: $(this).text(), pathName: $(this).attr('path-name')});
+        });
+    }
+    return attachments;
 }
