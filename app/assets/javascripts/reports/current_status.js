e9734ddd85a462ce55e108221b0814d317224f55
pptalent <wayneup@163.com>
2014-09-05 13:37:14 +0800

    module for grid

--- app/assets/javascripts/reports/current_status.js
+++ app/assets/javascripts/reports/current_status.js
@@ -2,4 +2,9 @@ var current_status={};
 current_status.init=function(container_id,data){
     DV.init(container_id);
     DV.parse(data["Vehicle_1"]);
+    $("#vechile-select").change(function () {
+        DV.clear();
+        var a = $("#vechile-select option:selected").text();
+        DV.parse(vdata[a]);
+    });
 }
--- app/assets/javascripts/reports/main.js
+++ app/assets/javascripts/reports/main.js
@@ -5,11 +5,6 @@
         window.setInterval(function(){
             $("#current-clock").text(format_time.current_time_clock());
         },1000);
-        current_status.init("data_container",vdata);
-        $("#vechile-select").change(function () {
-            DV.clear();
-            var a = $("#vechile-select option:selected").text();
-            DV.parse(vdata[a]);
-        });
+//        current_status.init("data_container",vdata);
     })
 })()
