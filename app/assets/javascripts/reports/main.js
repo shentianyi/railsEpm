56b0f743e1a342648d4f9c6fec10d13c33911226
Tesla <leechee89@gmail.com>
2014-09-08 01:01:36 +0800

    fix

--- app/assets/javascripts/reports/main.js
+++ app/assets/javascripts/reports/main.js
@@ -5,27 +5,28 @@
         window.setInterval(function(){
             $("#current-clock").text(format_time.current_time_clock());
         },1000);
-        current_status.init("data_container",vdata);
+        //current_status.init("data_container",vdata);
 
         loader = new SVGLoader( document.getElementById( 'preloader' ), { speedIn : 100 } );
-        $('body').on("click","#my-reports a",function(){
+        $('body').on("click","#my-reports a",function() {
             $("#my-reports li a").removeClass("active");
             $(this).find("a").addClass("active");
-            var now_href=window.location.href.split("/");
-            var length=now_href.length;
-            var left=document.getElementById("report-menu").getBoundingClientRect().right,
-                top=document.getElementById("report-menu").getBoundingClientRect().top>=0?document.getElementById("report-menu").getBoundingClientRect().top:0;
-            $(".pageload-overlay svg").css('left',left);
-            $(".pageload-overlay svg").css('top',top);
+            var now_href = window.location.href.split("/");
+            var length = now_href.length;
+            var left = document.getElementById("report-menu").getBoundingClientRect().right,
+                top = document.getElementById("report-menu").getBoundingClientRect().top >= 0 ? document.getElementById("report-menu").getBoundingClientRect().top : 0;
+            $(".pageload-overlay svg").css('left', left);
+            $(".pageload-overlay svg").css('top', top);
             loader.show();
-            setTimeout( function() {
+            setTimeout(function () {
                 loader.hide();
 
-            }, 2000 );
+            }, 2000);
+        });
 
         var current_type = 'current-status'
         var option = {};
-        switch(current_type){
+        switch(current_type) {
             case 'current-status':
                 option.type = 'current-status';
                 option.container = 'data_container';
@@ -37,8 +38,8 @@
                 option.type = 'current-status';
                 option.container = 'data-container';
                 break;
-
         }
+
         Report.init(option);
     })
 })()
