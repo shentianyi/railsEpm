db28d77e17df00e4a5b6b9e6ac2634fefc4adee0
pptalent <wayneup@163.com>
2014-09-07 23:36:45 +0800

    preload && dataview

--- app/assets/javascripts/reports/main.js
+++ app/assets/javascripts/reports/main.js
@@ -5,7 +5,19 @@
         window.setInterval(function(){
             $("#current-clock").text(format_time.current_time_clock());
         },1000);
-//        current_status.init("data_container",vdata);
-        grid.init();
+        current_status.init("data_container",vdata);
+//        grid.init();
+        loader = new SVGLoader( document.getElementById( 'preloader' ), { speedIn : 100 } );
+        $('body').on("click","#reports-center-list a",function(){
+            var left=document.getElementById("report-menu").getBoundingClientRect().right,
+                top=document.getElementById("report-menu").getBoundingClientRect().top>=0?document.getElementById("report-menu").getBoundingClientRect().top:0;
+            $(".pageload-overlay svg").css('left',left);
+            $(".pageload-overlay svg").css('top',top);
+            loader.show();
+            setTimeout( function() {
+                loader.hide();
+
+            }, 2000 );
+        });
     })
 })()
