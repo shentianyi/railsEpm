239477c3a4485a5c3f5a547f2027801df9d2d62a
tesla lee <leechee89@gmail.com>
2014-09-09 12:31:49 +0800

    fix

--- app/assets/javascripts/reports/data-current-status.js
+++ app/assets/javascripts/reports/data-current-status.js
@@ -5,13 +5,20 @@
         for(var j = 0;j<models.length;j++){
             var model = models[j];
             for(var i = 0;i<d_current_status[model].length;i++){
-                d_current_status[model]["Defects"] = d_current_status[model]["Defects"]+RAND.rate(10,80,0);
-                d_current_status[model]["Pass"] = d_current_status[model]["Pass"] + RAND.rate(5,80,0);
-                var rate = Math.floor(Math.random()*2-1);
-                d_current_status[model]["FTQ"] =   d_current_status[model]["FTQ"] + rate;
+                var old = parseInt(d_current_status[model][i]["Defects"]);
+                d_current_status[model][i]["Defects"] = (old+RAND.rate(1,20,0)).toString();
+
+                old = parseInt(d_current_status[model][i]["Pass"])
+                d_current_status[model][i]["Pass"] = (old + RAND.rate(1,80,0)).toString();
+
+                var rate = Math.floor(Math.random()*3-1);
+                old = parseInt(d_current_status[model][i]["FTQ"])
+                if(old+rate<100){
+                    d_current_status[model][i]["FTQ"] = (old+rate).toString();
+                }
             }
         }
-    },1000);
+    },5000);
 }())
 
 var d_current_status = {
