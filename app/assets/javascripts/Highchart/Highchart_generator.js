f402a9710125357468ca16c14cebd22f3a45d56c
pptalent <wayneup@163.com>
2014-09-10 10:21:30 +0800

    table

--- app/assets/javascripts/Highchart/Highchart_generator.js
+++ app/assets/javascripts/Highchart/Highchart_generator.js
@@ -14,7 +14,124 @@ var Highchart_generator=function(option){
             this.basic.chart.height=option.height
         }
         $target.highcharts(this.basic);
-    }
+    };
+    this.basic={
+        chart: {
+            borderRadius:0,
+            animation: {
+                duration: 800
+            }
+        },
+        colors:[
+            '#97cbe4',
+            '#f99c92',
+            '#81dfcd',
+            '#ffdb6d',
+            '#82d9e7',
+            '#dabeea',
+            '#6485a7',
+            '#f9b360',
+            '#94cd7b',
+            '#69b0bd'
+        ],
+        title: {
+            style:{
+                'fontSize':"24px",
+                color:'#555'
+            },
+            margin:35,
+            align:"center"
+        },
+        subtitle: {
+            style:{
+                'fontSize':"17px",
+                color:'#666'
+            },
+            floating:true,
+            y:36,
+            align:"center"
+        },
+        credits: {
+            enabled: false
+        },
+        exporting:{
+            enabled:false
+        },
+        tooltip:{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, 'rgba(96, 96, 96, .8)'],
+                    [1, 'rgba(16, 16, 16, .8)']
+                ]
+            },
+            borderWidth: 0,
+            style: {
+                color: '#FFF'
+            },
+            formatter: function() {
+                var name=this.series.name==="Target"?"Target":"Current";
+                return '<b>'+name+'</b>'
+                    +'<br />Value: <span style="color:'+this.series.color+'">'+this.point.y
+                    +'</span>'
+
+            }
+        },
+        legend: {
+            enabled: true,
+            borderRadius: 2,
+            borderColor: "rgba(0,0,0,0)",
+            animation: true,
+            maxHeight: 40,
+            margin:0,
+            itemMarginBottom: -2,
+            itemStyle: {
+                color: '#777'
+            },
+            itemHoverStyle: {
+                color: '#bbb'
+            },
+            itemHiddenStyle: {
+                color: '#ccc'
+            }
+        },
+        xAxis: {
+            lineWidth: 0,
+            tickWidth: 1,
+            offset: 0,
+            labels: {
+                style: {
+                    color:'rgba(0,0,0,0.4)',
+                    fontWeight:"bold"
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                enabled: false
+            },
+            labels: {
+                style: {
+                    color:'rgba(0,0,0,0.4)',
+                    fontWeight: 'bold'
+                }
+            },
+            tickPixelInterval: 30,
+            gridLineDashStyle: 'solid',
+            gridLineWidth:1,
+            gridLineColor: 'rgba(0,0,0,0.3)'
+        },
+        plotOptions: {
+            series: {
+                shadow: true
+            },
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        }
+    };
     this.init_daily_dpv=function(){
         var $target=this.target.indexOf("#")===-1?$("#"+this.target):$(this.target);
         this.basic.title.text=option.title?option.title:null;
@@ -49,7 +166,7 @@ var Highchart_generator=function(option){
             this.basic.colors[0]=this.color_system[option.color]?this.color_system[option.color]:this.color_system["blue"];
         }
         $target.highcharts(this.basic);
-    }
+    };
     this.reload_daily_dpv=function(option){
         var $target=this.target.indexOf("#")===-1?$("#"+this.target):$(this.target);
         $target.highcharts().destroy();
@@ -57,8 +174,9 @@ var Highchart_generator=function(option){
             this.basic.xAxis.categories=option.xArray;
         }
         this.basic.series=option.data;
+        console.log(this.basic)
         $target.highcharts(this.basic);
-    }
+    };
     this.resize=function(width,height){
         var $target=this.target.indexOf("#")===-1?$("#"+this.target):$(this.target),
             chart=$target.highcharts();
@@ -67,122 +185,6 @@ var Highchart_generator=function(option){
             height,
             false
         );
-    }
-}
-Highchart_generator.prototype.basic={
-    chart: {
-        borderRadius:0,
-        animation: {
-            duration: 800
-        }
-    },
-    colors:[
-        '#97cbe4',
-        '#f99c92',
-        '#81dfcd',
-        '#ffdb6d',
-        '#82d9e7',
-        '#dabeea',
-        '#6485a7',
-        '#f9b360',
-        '#94cd7b',
-        '#69b0bd'
-    ],
-    title: {
-        style:{
-            'fontSize':"24px",
-            color:'#555'
-        },
-        margin:35,
-        align:"center"
-    },
-    subtitle: {
-        style:{
-            'fontSize':"17px",
-            color:'#666'
-        },
-        floating:true,
-        y:36,
-        align:"center"
-    },
-    credits: {
-        enabled: false
-    },
-    exporting:{
-        enabled:false
-    },
-    tooltip:{
-        backgroundColor: {
-            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-            stops: [
-                [0, 'rgba(96, 96, 96, .8)'],
-                [1, 'rgba(16, 16, 16, .8)']
-            ]
-        },
-        borderWidth: 0,
-        style: {
-            color: '#FFF'
-        },
-        formatter: function() {
-            var name=this.series.name==="Target"?"Target":"Current";
-            return '<b>'+name+'</b>'
-                +'<br />Value: <span style="color:'+this.series.color+'">'+this.point.y
-                +'</span>'
+    };
 
-        }
-    },
-    legend: {
-        enabled: true,
-        borderRadius: 2,
-        borderColor: "rgba(0,0,0,0)",
-        animation: true,
-        maxHeight: 40,
-        margin:0,
-        itemMarginBottom: -2,
-        itemStyle: {
-            color: '#777'
-        },
-        itemHoverStyle: {
-            color: '#bbb'
-        },
-        itemHiddenStyle: {
-            color: '#ccc'
-        }
-    },
-    xAxis: {
-        lineWidth: 0,
-        tickWidth: 1,
-        offset: 0,
-        labels: {
-            style: {
-                color:'rgba(0,0,0,0.4)',
-                fontWeight:"bold"
-            }
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            enabled: false
-        },
-        labels: {
-            style: {
-                color:'rgba(0,0,0,0.4)',
-                fontWeight: 'bold'
-            }
-        },
-        tickPixelInterval: 30,
-        gridLineDashStyle: 'solid',
-        gridLineWidth:1,
-        gridLineColor: 'rgba(0,0,0,0.3)'
-    },
-    plotOptions: {
-        series: {
-            shadow: true
-        },
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    }
 }
--- app/assets/javascripts/reports/daily_dpv.js
+++ app/assets/javascripts/reports/daily_dpv.js
@@ -14,7 +14,6 @@ daily_dpv.init=function(){
     };
     var chart_one=new Highchart_generator(option_one);
     chart_one.init_daily_dpv();
-
     var option_two={
         target:"chart_dpv_two",
         xArray:["iQ1","iQ2","iQ IP","iQ DR","iQ3","iQ4","iQ5","iQ6","iQ7","iQ8","iQ9","iQ10","iQ11","iQ12","iQ13","iQ14","iQ15"],
@@ -28,24 +27,23 @@ daily_dpv.init=function(){
     };
     var chart_two=new Highchart_generator(option_two);
     chart_two.init_daily_dpv();
-    daily_dpv.flexible();
-
-
-    window.onresize=function(){
-        daily_dpv.flexible();
-    }
-
     this.chart_dpv = chart_one;
     this.chart_sdpv = chart_two;
 
+    daily_dpv.flexible();
+    window.onresize=function(){
+        daily_dpv.flexible();
+    }
 }
 daily_dpv.flexible=function(){
+    //set the content whole height
     var total_height=$("#wrap-main").height()-$("header").height()-1;
+    //set the table height and width
     $("#report-content").height(total_height);
-//    var chart_layout_height= total_height-$("#snap-groups").height();
-//    $("#chart_layout").height(chart_layout_height);
-//    var height=total_height-$("#chart_dpv_one").height()-$("#chart_dpv_two").height()-2;
-//    $("#data_container").width($("#report-content .left").width());
-//    $("#data_container").height(height);
-
+    var chart_one_height=$("#chart_dpv_one").height(),
+        chart_two_height=$("#chart_dpv_two").height();
+    $("#data_container").height(total_height-chart_one_height-chart_two_height-15);
+    $("#data_container").width($("#data_container").width()-2);
+    // left right same height
+    $("#report-content .right").height($("#report-content .left").height());
 }
