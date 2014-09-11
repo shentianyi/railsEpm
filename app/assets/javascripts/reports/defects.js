ac44135a7275b4b0bc29676e241955efa7885a39
wayne <zixiao.wang@cz-tek.com>
2014-09-11 12:20:03 +0800

    defects

--- app/assets/javascripts/reports/defects.js
+++ app/assets/javascripts/reports/defects.js
@@ -1,36 +1,33 @@
-<div id="defects" class="data_container" >
-</div>
-<script>
-$(document).ready(function(){
-	var width = 70;
-	var widthstring = "";
-	var alienstring = "";
-	var coltypestring = "";
-	var colsortstring = "";
-	var headers = "Model Group,VIN,NAME,Level 1,Level 2,Level3,Level4,Level5,Fault Description" +
-            ",Station,Rank,Res.Dep,Inspector,repaired by,confirmed by,input date/time,Shift,Package code,Pilot";
+var defects={};
+defects.example_init=function(){
+    var width = Math.floor(($("#report-content").width()-20)/19-3);
+    var widthstring = "";
+    var alienstring = "";
+    var coltypestring = "";
+    var colsortstring = "";
+    var headers = "Model Group,VIN,NAME,Level 1,Level 2,Level3,Level4,Level5,Fault Description" +
+        ",Station,Rank,Res.Dep,Inspector,repaired by,confirmed by,input date/time,Shift,Package code,Pilot";
     var length = headers.split(",").length;
-	for(var i = 0;i<length;i++){
-		if(i<18){
-			alienstring = alienstring+"center,"
-			widthstring = widthstring+width+",";
-			coltypestring = coltypestring + "ro,";
-			colsortstring = colsortstring + "str,";
-		}else{
-			alienstring = alienstring+"center";
-			widthstring = widthstring+width;
-			coltypestring = coltypestring + "ro";
+    for(var i = 0;i<length;i++){
+        if(i<18){
+            alienstring = alienstring+"center,"
+            widthstring = widthstring+width+",";
+            coltypestring = coltypestring + "ro,";
+            colsortstring = colsortstring + "str,";
+        }else{
+            alienstring = alienstring+"center";
+            widthstring = widthstring+width;
+            coltypestring = coltypestring + "ro";
             colsortstring = colsortstring + "str";
-		}
-		
-	}
+        }
 
-	var defectsgrid = new dhtmlXGridObject("defects");
+    }
+
+    var defectsgrid = new dhtmlXGridObject("all_defects");
     defectsgrid.setImagePath("/assets/dhtmlx/");
     defectsgrid.setHeader(headers);
     defectsgrid.setInitWidths(widthstring);
     defectsgrid.enableAutoWidth(false);
-
     defectsgrid.setColAlign(alienstring);
     defectsgrid.setColTypes(coltypestring);
     defectsgrid.setColSorting(coltypestring);
@@ -38,7 +35,6 @@ $(document).ready(function(){
     defectsgrid.setColumnColor("#d5f1ff");
     defectsgrid.enableSmartRendering(true);
     defectsgrid.init();
-
     var data = {
         rows:[{
             id:"1",
@@ -46,9 +42,7 @@ $(document).ready(function(){
             ]
         }]
     };
-
     var stations = ["BBO","PBO","DBO","IBO","MTR","INT","TBO","MCH","CBO","U/H","EXT","ELE","GBO","ROL","SHT","OKL","ROD","EOL"];
-
     for(var i = 0;i<100;i++){
         data["rows"][i] ={
             id: i+1,
@@ -75,7 +69,23 @@ $(document).ready(function(){
             ]
         };
     };
+    defectsgrid.clearAll();
+    defectsgrid.parse(data,'json');
 
+
+
+    var key_defectsgrid = new dhtmlXGridObject("key_defects");
+    key_defectsgrid.setImagePath("/assets/dhtmlx/");
+    key_defectsgrid.setHeader(headers);
+    key_defectsgrid.setInitWidths(widthstring);
+    key_defectsgrid.enableAutoWidth(false);
+    key_defectsgrid.setColAlign(alienstring);
+    key_defectsgrid.setColTypes(coltypestring);
+    key_defectsgrid.setColSorting(coltypestring);
+    key_defectsgrid.setSkin("dhx_skyblue");
+    key_defectsgrid.setColumnColor("#d5f1ff");
+    key_defectsgrid.enableSmartRendering(true);
+    key_defectsgrid.init();
     var keydata = {
         rows:[{
             id:"1",
@@ -83,7 +93,6 @@ $(document).ready(function(){
             ]
         }]
     };
-
     var key_stations = ["iQ1","iQ2","iQ11","EOL"];
     for(var i = 0;i<100;i++){
         keydata["rows"][i] ={
@@ -111,8 +120,23 @@ $(document).ready(function(){
             ]
         };
     };
+    key_defectsgrid.clearAll();
+    key_defectsgrid.parse(keydata,'json');
 
-    defectsgrid.clearAll();
-    defectsgrid.parse(keydata,'json');
-});
-</script>
+    defects.resize("all_defects");
+    defects.resize("key_defects");
+    window.onresize=function(){
+        defects.resize("all_defects");
+        defects.resize("key_defects");
+    }
+}
+defects.resize=function(target){
+   var $target=target.indexOf("#")===-1?$("#"+target):$(target),
+       total_height=$("#wrap-main").height()-$("header").height()- 1,
+       table_height=total_height-$(".wrapper-header").height()-$target.find(".xhdr").height()-2,
+       height=total_height-$(".wrapper-header").height() - 2,
+       width=$("#report-content").width()-10;
+   $target.find(".objbox").css("height",table_height).css("width",width);
+   $target.css("width",width).css("height",height);
+   $target.find(".xhdr").css("width",width);
+}
