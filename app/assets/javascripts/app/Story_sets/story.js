b6f7bfce7bdd221263316c4a8d3e6382c86ed005
wayne <wayneup@163.com>
2015-01-12 17:54:55 +0800

    discussion

--- app/assets/javascripts/app/Story_sets/story.js
+++ app/assets/javascripts/app/Story_sets/story.js
@@ -8,10 +8,22 @@ define(["jquery","jquery.scroll"],function($){
             axis:"y",
             theme:"minimal-dark"
         });
+        $("#conversation-list").mCustomScrollbar({
+            axis:"y",
+            theme:"dark"
+        });
+        $("#people").mCustomScrollbar({
+            axis:"x", // horizontal scrollbar
+            advanced:{
+                autoExpandHorizontalScroll: true
+            },
+            theme:"rounded-dark"
+        });
     }
     return{
         init:function(){
             initScroll();
+
         }
     }
 })
