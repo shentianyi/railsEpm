7362fbb608e66cecf373dad365a4e4f10f1d35b2
Charlot <7wangsong@gmail.com>
2015-05-26 11:27:25 +0800

    fix create tenant error

--- app/controllers/admin/tenants_controller.rb
+++ app/controllers/admin/tenants_controller.rb
@@ -29,16 +29,17 @@ class Admin::TenantsController < Admin::ApplicationController
   end
 
   def create
-    begin
+    puts params.to_json
+    # begin
       @user=User.new
       @user.status=0
       @user.first_name=params[:user][:first_name]
-      @user.create_tenant_user!(params[:user][:email], params[:user][:password], params[:user][:password], params[:tenant][:company_name])
+      @user.create_tenant_user!(params[:user][:first_name],params[:user][:email], params[:user][:password], params[:user][:password], params[:tenant][:company_name])
       @tenant=@user.tenant
       redirect_to  [:admin,@tenant], notice: 'Tenant was successfully created.'
-    rescue Exception => e
-      render action: "new", notice: "created error: #{e.message}"
-    end
+    # rescue Exception => e
+    #   render action: "new", notice: "created error: #{e.message}"
+    # end
   end
 
   def update
--- app/helpers/tenants_helper.rb
+++ app/helpers/tenants_helper.rb
@@ -1,17 +1,17 @@
 #encoding: utf-8
 module TenantsHelper
-    # init tenant when user reg for try
-    def self.init_tenant params
-	begin
-	    super_user=User.new(:first_name=>params[:first_name],:last_name=>params[:last_name],:is_tenant=>true,:password=>params[:password])
-	    tenant=Tenant.new(:company=>params[:company])
-	    super_user.tenant=tenant
-	    tenant.super_user=super_user
-	    tenant.save
-	rescue Exception=>e
-	    puts e.message
-	    return false
-	end
-	return true
+  # init tenant when user reg for try
+  def self.init_tenant params
+    begin
+      super_user=User.new(:first_name => params[:first_name], :last_name => params[:last_name], :is_tenant => true, :password => params[:password])
+      tenant=Tenant.new(:company => params[:company])
+      super_user.tenant=tenant
+      tenant.super_user=super_user
+      tenant.save
+    rescue Exception => e
+      puts e.message
+      return false
     end
+    return true
+  end
 end
