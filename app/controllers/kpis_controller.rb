fe8649193cc18d462bcd0bb24e439c6a6d6e657e
Charlot <7wangsong@gmail.com>
2015-10-11 11:00:01 +0800

    change download kpi entry name

--- app/controllers/kpis_controller.rb
+++ app/controllers/kpis_controller.rb
@@ -222,7 +222,7 @@ class KpisController < ApplicationController
   def download_entry_template
     kpis=current_user.kpis
     msg=FileHandler::Excel::KpiHandler.download_entry_template(current_user, kpis)
-    send_file msg.content
+    send_file msg.content,filename:"#{current_user.first_name}_KPI输入模版.xlsx"
   end
 
   private
