49b9d6fa45dc7d19478499779c5146384a9ea13f
Charlot <7wangsong@gmail.com>
2015-10-28 11:35:09 +0800

    disable taskid & working_types i18n

--- app/api/v1/custom_api.rb
+++ app/api/v1/custom_api.rb
@@ -4,18 +4,18 @@ module V1
 
     namespace :custom do
       get :task_ids do
-        JSON.parse(File.read("data/tasks_#{locale}.json"))
+        JSON.parse(File.read("data/tasks_en.json"))
       end
 
       get :working_types do
-        JSON.parse(File.read("data/working_types_#{locale}.json"))
+        JSON.parse(File.read("data/working_types_en.json"))
       end
 
       get :user_info do
         puts params[:user_id]
         {
             user_id: current_user.email,
-            name: current_user.first_name.to_s + " " + current_user.last_name.to_s,
+            name: current_user.first_name.to_s, #+ " " + current_user.last_name.to_s,
             stuff_id: current_user.stuff_id,
             project_id: current_user.current_project_id,
             current_location: current_user.current_location,
