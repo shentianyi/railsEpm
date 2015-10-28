6e15d9084e80f8c6f2180dac2224b053b647dbe0
Charlot <7wangsong@gmail.com>
2015-10-28 18:37:59 +0800

    change string float

--- lib/active_support/core_ext/string/number.rb
+++ lib/active_support/core_ext/string/number.rb
@@ -1,8 +1,8 @@
 #encoding: utf-8
 class String
   def is_number?
-    # true if Float(self) rescue false
-    self.sub!(/[.0]+$/,'')
-    self.to_f.to_s == self.to_s || self.to_i.to_s == self.to_s
+    true if Float(self) rescue false
+    # self.sub!(/[.0]+$/,'')
+    # self.to_f.to_s == self.to_s || self.to_i.to_s == self.to_s
   end
 end
