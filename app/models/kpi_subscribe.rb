318da4b6ae3ce40fa5f0eecb26e6821f9c606b74
lzding <472339105@qq.com>
2016-01-22 09:51:07 +0800

    fix

--- app/models/kpi_subscribe.rb
+++ app/models/kpi_subscribe.rb
@@ -1,6 +1,6 @@
 class KpiSubscribe < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :user_id, :tenant_id, :kpi_id, :is_alert,:alert_by_sms,:alert_by_email
+  attr_accessible :user_id, :tenant_id, :kpi_id, :is_alert, :alert_by_sms, :alert_by_email
   belongs_to :user
   belongs_to :tenant
   belongs_to :kpi
@@ -11,18 +11,20 @@ class KpiSubscribe < ActiveRecord::Base
 
   #在每个KpiEntry被更新或者修改时出发
   def execute kpi_entry
-    if self.chart_condition.check(kpi_entry) && !self.is_alert
-      query = AnalyseService.chart_condition_filter(self.chart_condition)
-      query[:start_time] = kpi_entry.parsed_entry_at.to_s
-      query[:end_time] = kpi_entry.parsed_entry_at.to_s
-      data = Entry::Analyzer.new(query).analyse
-      self.kpi_subscribe_alerts.each do |alert|
-        if alert.execute data[:current]
-          alert_user
-          puts 'ALERT================='
-          self.is_alert = true
-          self.save
-          return
+    unless self.chart_condition.blank?
+      if self.chart_condition.check(kpi_entry) && !self.is_alert
+        query = AnalyseService.chart_condition_filter(self.chart_condition)
+        query[:start_time] = kpi_entry.parsed_entry_at.to_s
+        query[:end_time] = kpi_entry.parsed_entry_at.to_s
+        data = Entry::Analyzer.new(query).analyse
+        self.kpi_subscribe_alerts.each do |alert|
+          if alert.execute data[:current]
+            alert_user
+            puts 'ALERT================='
+            self.is_alert = true
+            self.save
+            return
+          end
         end
       end
     end
