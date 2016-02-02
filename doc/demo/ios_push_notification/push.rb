2b63f456547b86062b2a42a6abd2a68f395e45b1
Charlot <7wangsong@gmail.com>
2016-02-02 13:57:15 +0800

    change

--- doc/demo/ios_push_notification/push.rb
+++ doc/demo/ios_push_notification/push.rb
@@ -3,10 +3,10 @@ require 'houston'
 # Environment variables are automatically read, or can be overridden by any specified options. You can also
 # conveniently use `Houston::Client.development` or `Houston::Client.production`.
 APN = Houston::Client.development
-APN.certificate = File.read('./PNKey.pem')
-
+APN.certificate = File.read('./PushKey.pem')
+#APN.certificate = File.read('./old/PNKey.pem')
 # An example of the token sent back when a device registers for notifications
-token = "<9e446ff5 c730904f 6bb6619e ad4b893e e8f4dee5 f452e799 4bbd86fd a4ff7141>"
+token = '<dc04a9e2 d6a59824 8a1f203d 2b0e1e17 ac5dcd56 b8b134d2 c70d1754 203351da>'
 
 # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
 notification = Houston::Notification.new(device: token)
