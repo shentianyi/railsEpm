d2a565c7f70be13731d6a01f6e20df1eb6fc280e
Charlot <7wangsong@gmail.com>
2016-03-26 23:00:57 +0800

    update

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -335,14 +335,14 @@ class DepartmentsController < ApplicationController
     if (eg=EntityGroup.find_by_id(params[:id])) && (e=eg.entities.first)
       q = KpiEntry.where(kpi_id: Kpi.first.id,
                          entity_id: e.id)
-      start_time=Time.parse(params[:start_time]).utc#.to_s
-      end_time=(Time.parse(params[:end_time])-1.second).utc#.to_s
+      start_time=Time.parse(params[:start_time]).utc #.to_s
+      end_time=(Time.parse(params[:end_time])-1.second).utc #.to_s
       p start_time
       p end_time
       q=q.between(Hash[:entry_at, (start_time..end_time)])
 
       q=q.order_by(entry_at: :asc)
-      
+
       q.each do |entry|
         entries<<{
             id: entry._id.to_s,
@@ -352,10 +352,17 @@ class DepartmentsController < ApplicationController
             target_min: kpi.target_min
         }
       end
-
     end
 
-    render json: entries
-
+    if e
+      render json: {
+                 id: e.id,
+                 name: eg.name,
+                 data: entries
+             }
+    else
+      render json: nil
+    end
   end
+
 end
