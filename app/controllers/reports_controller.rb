78eb5aaf29278bee546ec8d9e20011a04ad8ae9a
Charlot <7wangsong@gmail.com>
2016-05-19 09:49:49 +0800

    change

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,14 +2,28 @@ class ReportsController < ApplicationController
   skip_authorize_resource
 
   def index
-    params[:part] = '1' unless params.has_key? 'part'
-    @partial = ChartType.partial(params[:part].to_i)
-    # snaps
-    #@partial='grid'
-    @users=User.where('id <> ?', current_user.id).where(role_id: Role.director).all
-    @snaps=current_user.report_snaps.where(type: params[:part]).order('created_at desc').all
-    render :partial => @partial if params[:ajax]
+    if params[:from_ipad].present? && params[:ajax].blank?
+      @from_ipad=true
+      @part=params[:part]
+      respond_to do |format|
+        format.html { render 'ipad', :layout => 'app_application' }
+        format.json
+      end
+    else
+
+    end
+
+    if params[:ajax]
+      params[:part] = '1' unless params.has_key? 'part'
+      @partial = ChartType.partial(params[:part].to_i)
+      # snaps
+      #@partial='grid'
+      #@users=User.where('id <> ?', current_user.id).where(role_id: Role.director).all
+      #@snaps=current_user.report_snaps.where(type: params[:part]).order('created_at desc').all
+      render :partial => @partial
+    end
   end
+
 
   def subscription
     render :partial => "subscription"
