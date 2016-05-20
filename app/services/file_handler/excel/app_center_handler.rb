317cf3bda0db5c9cb27b03240cd65fc0bd01fe4d
Charlot <7wangsong@gmail.com>
2016-05-20 11:07:44 +0800

    change

--- app/services/file_handler/excel/app_center_handler.rb
+++ app/services/file_handler/excel/app_center_handler.rb
@@ -7,36 +7,40 @@ module FileHandler
       def self.download_cycle_time_detail(params)
         msg = Message.new
         begin
-          tmp_file = full_export_path("CycleTime.xlsx") unless tmp_file
-          p = Axlsx::Package.new
+        tmp_file = full_export_path("CycleTime.xlsx") unless tmp_file
+        p = Axlsx::Package.new
 
-          parent=Department.find_by_id(params[:product_line])
-          kpi=Kpi.first
-          start_time=Time.parse(params[:start_time]).utc #.to_s
-          end_time=(Time.parse(params[:end_time])-1.second).utc #.to_s
+        parent=Department.find_by_id(params[:product_line])
+        kpi=Kpi.first
+        start_time=Time.parse(params[:start_time]).utc #.to_s
+        end_time=(Time.parse(params[:end_time])-1.second).utc #.to_s
 
-          p.workbook.add_worksheet(:name => "Total Count Worksheet") do |sheet|
-            if kpi && parent
+        p.workbook.add_worksheet(:name => "Total Count Worksheet") do |sheet|
+          if kpi && parent
 
-              sheet.add_row ['Name', 'Code', 'Value'], types: [:string, :string, :string]
+            sheet.add_row ['Name', 'Code', 'Value'], types: [:string, :string, :string]
 
-              parent.children.each do |d|
-                eg=d.entity_group
+            parent.children.each do |d|
+
+              eg=d.entity_group
+              if eg
                 e= eg.entities.first
-                q = KpiEntry.where(kpi_id: Kpi.first.id,entity_id: e.id)
+                q = KpiEntry.where(kpi_id: Kpi.first.id, entity_id: e.id)
 
                 q=q.between(Hash[:entry_at, (start_time..end_time)])
                 sheet.add_row [eg.name, e.code, q.count], types: [:string, :string, :string]
               end
-            else
-              sheet.add_row ['No Data']
             end
+          else
+            sheet.add_row ['No Data']
           end
+        end
 
 
-          if kpi && parent
-            parent.children.each do |d|
-              eg=d.entity_group
+        if kpi && parent
+          parent.children.each do |d|
+            eg=d.entity_group
+            if eg
               e= eg.entities.first
               p.workbook.add_worksheet(:name => eg.name) do |sheet|
 
@@ -48,19 +52,20 @@ module FileHandler
 
                 q.each do |entry|
                   sheet.add_row [
-                      entry.entry_at,
-                      KpiUnit.parse_entry_value(kpi.unit, entry.value),
-                  ], types:[:string, :string]
+                                    entry.entry_at,
+                                    KpiUnit.parse_entry_value(kpi.unit, entry.value),
+                                ], types: [:string, :string]
                 end
               end
             end
           end
+        end
 
-          p.use_shared_strings = true
-          p.serialize(tmp_file)
+        p.use_shared_strings = true
+        p.serialize(tmp_file)
 
-          msg.result =true
-          msg.content =tmp_file
+        msg.result =true
+        msg.content =tmp_file
         rescue => e
           msg.content = e.message
         end
