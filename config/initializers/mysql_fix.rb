4970ae877f47f2c99a2d7dee58770f2eab5ea6be
SongFuZhen <evil_song@126.com>
2016-05-24 17:23:53 +0800

    fixed flexbox

--- config/initializers/mysql_fix.rb
+++ config/initializers/mysql_fix.rb
@@ -1,7 +1,7 @@
 module ActiveRecord::ConnectionAdapters
   class Mysql2Adapter
     alias_method :execute_without_retry, :execute
-
+    NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
     def execute(*args)
       execute_without_retry(*args)
     rescue ActiveRecord::StatementInvalid => e
